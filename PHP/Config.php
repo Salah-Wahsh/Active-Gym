38a0f8fc785b6fedb32775f99271e1eae36bbf00
Salah-Wahsh <s.m.abdallah@student.aast.edu>
2022-03-17 15:51:36 +0200

    db1

--- PHP/Config.php
+++ PHP/Config.php
@@ -1,9 +1,9 @@
 <?php
 
-$Host = "localhost";
-$Username = "root";
-$Password = "";
-$DB_Name = "active";
+$Host = "us-cdbr-east-05.cleardb.net";
+$Username = "bce73203446537";
+$Password = "1ecc5223";
+$DB_Name = "heroku_668576cf359d5f1";
 
 
 $con = mysqli_connect($Host, $Username, $Password, $DB_Name) or die("couldn't Connect");
